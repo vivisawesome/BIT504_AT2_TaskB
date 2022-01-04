01bdf88b29c9a3ce64db80c5d535a46771232f79
BIT504 <wgao250@gmail.com>
2022-01-04 16:37:09 +1300

    Adding timing in PongPanel constructor, import javax.swing.Timer

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -1,15 +1,19 @@
 import java.awt.Color;
-   import java.awt.event.ActionEvent;
-   import java.awt.event.ActionListener;
-   import java.awt.event.KeyEvent;
-   import java.awt.event.KeyListener;
-   import javax.swing.JPanel;
-     
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+ 
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
       private final static Color BACKGROUND_COLOR = Color.BLACK;
+      private final static int TIMER_DELAY = 5;
  
       public PongPanel() {
           setBackground(BACKGROUND_COLOR);
+          Timer timer = new Timer(TIMER_DELAY,this);
+            timer.start();
       }
   
       @Override
