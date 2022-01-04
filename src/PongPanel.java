a9f27246dc38824d58addbe729313c068ef5932f
BIT504 <wgao250@gmail.com>
2022-01-04 16:48:30 +1300

    Add the update() method to Pongpanel

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -9,7 +9,8 @@ import javax.swing.Timer;
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
       private final static Color BACKGROUND_COLOR = Color.BLACK;
       private final static int TIMER_DELAY = 5;
- 
+      
+
       public PongPanel() {
           setBackground(BACKGROUND_COLOR);
           Timer timer = new Timer(TIMER_DELAY,this);
@@ -27,9 +28,14 @@ import javax.swing.Timer;
       @Override
       public void keyTyped(KeyEvent event) {
       }
+      
+      private void update() {
+    	  
+      }
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
       }
   
  }
